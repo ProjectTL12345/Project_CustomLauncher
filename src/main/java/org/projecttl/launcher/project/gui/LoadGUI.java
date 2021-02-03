71447399e31cb31a7f7ce0b59c7711a2ba82ad2f
Project_TL <kimpiao020@gmail.com>
2021-02-03 15:13:28 +0900

    init: Translate Kotlin later.

---
+++ src/main/java/org/projecttl/launcher/project/gui/LoadGUI.java
@@ -0,0 +1,16 @@
+package org.projecttl.launcher.project.gui;
+
+import javax.swing.*;
+
+public class LoadGUI {
+    private JPanel panel1;
+    private JButton loginButton;
+    private JPasswordField pwField;
+    private JTextField idField;
+    private JLabel pwLabel;
+    private JLabel idLabel;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
